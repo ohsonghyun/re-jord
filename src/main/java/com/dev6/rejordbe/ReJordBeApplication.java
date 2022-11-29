916926b936802e8049142d7b7a1a339aaaef6e66
flowertaekk-dev <flowertaekk.dev@gmail.com>
2022-11-29 21:07:15 +0900

    first commit

---
+++ src/main/java/com/dev6/rejordbe/ReJordBeApplication.java
@@ -0,0 +1,13 @@
+package com.dev6.rejordbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReJordBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReJordBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev6/rejordbe/ReJordBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev6.rejordbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReJordBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
