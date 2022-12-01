5f9788771a8c4ded7e58be025df5e16cc087817b
flowertaekk-dev <flowertaekk.dev@gmail.com>
2022-12-01 22:55:26 +0900

    feat(signup): findUserByNickname() 추가

--- src/test/groovy/com/dev6/rejordbe/infrastructure/user/UsersRepositorySpec.groovy
+++ src/test/groovy/com/dev6/rejordbe/infrastructure/user/UsersRepositorySpec.groovy
@@ -51,4 +51,33 @@ class UsersRepositorySpec extends Specification {
         userId   | email             | nickname   | password
         "userId" | "email@email.com" | "nickname" | "password"
     }
+
+    def "닉네임으로 유저를 찾을 수 있다"() {
+        given:
+        def newUser = Users.builder()
+                .userId(userId)
+                .email(email)
+                .nickname(nickname)
+                .password(password)
+                .build()
+
+        when:
+        usersRepository.save(newUser)
+
+        entityManager.flush()
+        entityManager.clear()
+
+        then:
+        Optional<Users> optionalAnUser = usersRepository.findUserByNickname(nickname)
+        optionalAnUser.isPresent()
+        def anUser = optionalAnUser.orElseThrow()
+        anUser.getUserId() == userId
+        anUser.getEmail() == email
+        anUser.getNickname() == nickname
+        anUser.getPassword() == password
+
+        where:
+        userId   | email             | nickname   | password
+        "userId" | "email@email.com" | "nickname" | "password"
+    }
 }
