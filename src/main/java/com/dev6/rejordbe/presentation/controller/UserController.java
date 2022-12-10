85b1ba72597750fed869edaa76e72d77fa7d82e6
flowertaekk-dev <flowertaekk.dev@gmail.com>
2022-12-10 18:18:22 +0900

    config(signup):UserController Swagger 설정

--- src/main/java/com/dev6/rejordbe/presentation/controller/UserController.java
+++ src/main/java/com/dev6/rejordbe/presentation/controller/UserController.java
@@ -5,6 +5,7 @@ import com.dev6.rejordbe.exception.IllegalParameterException;
 import com.dev6.rejordbe.presentation.controller.dto.signup.SignUpRequest;
 import com.dev6.rejordbe.domain.user.dto.UserResult;
 import com.dev6.rejordbe.presentation.controller.dto.signup.SignUpResponse;
+import io.swagger.annotations.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * UserController
  */
+@Api(tags = "유저 컨트롤러")
 @RestController
 @RequestMapping("/v1/users")
 @lombok.RequiredArgsConstructor
@@ -22,8 +24,24 @@ public class UserController {
 
     private final SignUpService signUpService;
 
-    @PostMapping("/signup")
-    public ResponseEntity<SignUpResponse> signUp(@RequestBody final SignUpRequest signUpUserRequest) {
+    @ApiOperation(
+            value = "저장피드 추가",
+            nickname = "signUp",
+            notes = "회원가입API. 에러인 경우에는 errors 필드에만 값 설정 후 리스폰스",
+            response = SignUpResponse.class,
+            authorizations = {@Authorization(value = "TBD")},
+            tags = "Users")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "정상"),
+            @ApiResponse(code = 400, message = "정책 위반 데이터", response = SignUpResponse.class),
+            @ApiResponse(code = 409, message = "존재하는 유저ID 또는 닉네임", response = SignUpResponse.class)
+    })
+    @PostMapping(
+            value = "/signup",
+            produces = {"application/json"},
+            consumes = {"application/json"}
+    )
+    public ResponseEntity<SignUpResponse> signUp(@ApiParam(value = "회원가입 정보", required = true) @RequestBody final SignUpRequest signUpUserRequest) {
         UserResult savedResult = signUpService.signUp(signUpUserRequest.toUser());
 
         // 에러. 회원가입 실패
@@ -34,8 +52,8 @@ public class UserController {
                                     .filter(ex -> ex instanceof IllegalParameterException)
                                     .findFirst()
                                     .isEmpty()
-                            ? HttpStatus.CONFLICT
-                            : HttpStatus.BAD_REQUEST)
+                                    ? HttpStatus.CONFLICT
+                                    : HttpStatus.BAD_REQUEST)
                     .body(
                             SignUpResponse.builder()
                                     .errors(
