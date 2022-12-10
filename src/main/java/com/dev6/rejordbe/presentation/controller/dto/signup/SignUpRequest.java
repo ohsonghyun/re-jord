bc9eefee9019877ac3f7041ba60e34f5ba5e7418
flowertaekk-dev <flowertaekk.dev@gmail.com>
2022-12-11 00:35:22 +0900

    config(signup): DTO swagger 설정

--- src/main/java/com/dev6/rejordbe/presentation/controller/dto/signup/SignUpRequest.java
+++ src/main/java/com/dev6/rejordbe/presentation/controller/dto/signup/SignUpRequest.java
@@ -2,6 +2,9 @@ package com.dev6.rejordbe.presentation.controller.dto.signup;
 
 import com.dev6.rejordbe.domain.user.UserType;
 import com.dev6.rejordbe.domain.user.Users;
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiParam;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.io.Serializable;
 
@@ -14,16 +17,21 @@ import java.io.Serializable;
 @lombok.Getter
 public class SignUpRequest implements Serializable {
 
+    @Schema(description = "유저 ID", required = true)
     private String userId;
-    private String nickname;
+
+    @Schema(description = "유저 패스워드", required = true)
     private String password;
+
+    @Schema(description = "유저 타입", required = true)
     private UserType userType;
 
-
+    /**
+     * SignUpRequest를 Users객체로 변환
+     */
     public Users toUser() {
         return Users.builder()
                 .userId(this.userId)
-                .nickname(this.nickname)
                 .password(this.password)
                 .userType(this.userType)
                 .build();
--- src/main/java/com/dev6/rejordbe/presentation/controller/dto/signup/SignUpResponse.java
+++ src/main/java/com/dev6/rejordbe/presentation/controller/dto/signup/SignUpResponse.java
@@ -2,20 +2,26 @@ package com.dev6.rejordbe.presentation.controller.dto.signup;
 
 import com.dev6.rejordbe.domain.user.UserType;
 import com.dev6.rejordbe.domain.user.Users;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 import java.io.Serializable;
 import java.util.List;
 
 /**
- * SignUpUserRequestDto
+ * SignUpResponse
  */
 @Builder
 @lombok.Getter
 public class SignUpResponse implements Serializable {
+    @Schema(description = "유저 UID")
     private String uid;
+    @Schema(description = "유저 ID")
     private String userId;
+    @Schema(description = "유저 닉네임")
     private String nickname;
+    @Schema(description = "유저 타입")
     private UserType userType;
+    @Schema(description = "에러(에러인 경우에만)")
     private List<String> errors;
 }
