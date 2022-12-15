bdf797f007eb8f491457637f81cc476d33e342f9
flowertaekk-dev <flowertaekk.dev@gmail.com>
2022-12-15 23:10:29 +0900

    feat: UserController 어드바이스 추가

---
+++ src/main/java/com/dev6/rejordbe/presentation/controller/dto/exception/ErrorResponse.java
@@ -0,0 +1,10 @@
+package com.dev6.rejordbe.presentation.controller.dto.exception;
+
+/**
+ * ErrorResponse
+ */
+@lombok.Builder
+@lombok.Getter
+public class ErrorResponse {
+    private String message;
+}
