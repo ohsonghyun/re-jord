9e99a2556e65691a23193fbe0da4ef30a75e8f36
flowertaekk-dev <flowertaekk.dev@gmail.com>
2022-12-28 21:23:55 +0900

    [add] 로그인 관련 repository 구현

--- src/main/java/com/dev6/rejordbe/infrastructure/user/UserInfoRepository.java
+++ src/main/java/com/dev6/rejordbe/infrastructure/user/UserInfoRepository.java
@@ -17,5 +17,14 @@ public interface UserInfoRepository extends JpaRepository<Users, String> {
      * @param nickname {@code String} 찾고자 하는 유저의 nickname
      * @return {@code Optional<Users>}
      */
-    Optional<Users> findUserByNickname(String nickname);
+    Optional<Users> findUserByNickname(final String nickname);
+
+    /**
+     * 유저ID and 패스워드로 유저 찾기
+     *
+     * @param userId {@code String} 찾고자 하는 유저의 userId
+     * @param password {@code String} 찾고자 하는 유저의 password
+     * @return {@code Optional<Users>}
+     */
+    Optional<Users> findUserByUserIdAndPassword(final String userId, final String password);
 }
--- src/test/groovy/com/dev6/rejordbe/infrastructure/user/UserInfoRepositorySpec.groovy
+++ src/test/groovy/com/dev6/rejordbe/infrastructure/user/UserInfoRepositorySpec.groovy
@@ -24,6 +24,68 @@ class UserInfoRepositorySpec extends Specification {
     @Autowired
     UserInfoRepository userInfoRepository
 
+    // ----------------------------------------------------
+    // 로그인 관련
+    // ----------------------------------------------------
+
+    def "유저를 UserId와 패스워드를 통해 검색할 수 있다"() {
+        given:
+        userInfoRepository.save(
+                Users.builder()
+                        .uid(uid)
+                        .userId(userId)
+                        .nickname(nickname)
+                        .password(password)
+                        .userType(userType)
+                        .build())
+        entityManager.flush()
+        entityManager.clear()
+
+        when:
+        def anUser = userInfoRepository.findUserByUserIdAndPassword(userId, password).orElseThrow()
+
+        then:
+        anUser != null
+        anUser.getUid() == uid
+        anUser.getUserId() == userId
+        anUser.getNickname() == nickname
+        anUser.getPassword() == password
+        anUser.getUserType() == userType
+
+        where:
+        uid   | userId   | nickname   | password   | userType
+        "uid" | "userId" | "nickname" | "password" | UserType.BASIC
+    }
+
+    def "유저를 UserId 또는 패스워드가 null인 경우에도 검색은 가능하다"() {
+        given:
+        userInfoRepository.save(
+                Users.builder()
+                        .uid('uid')
+                        .userId('userId')
+                        .nickname('nickname')
+                        .password('password')
+                        .userType(userType)
+                        .build())
+        entityManager.flush()
+        entityManager.clear()
+
+        when:
+        def userOptional = userInfoRepository.findUserByUserIdAndPassword(userId, password)
+
+        then:
+        userOptional.isEmpty()
+
+        where:
+        testCase             | uid   | userId   | nickname   | password   | userType
+        "userId가 null인 경우"   | "uid" | null     | "nickname" | "password" | UserType.BASIC
+        "password가 null인 경우" | "uid" | "userId" | "nickname" | null       | UserType.BASIC
+    }
+
+    // ----------------------------------------------------
+    // 닉네임 수정 관련
+    // ----------------------------------------------------
+
     def "유저 닉네임을 수정할 수 있다"() {
         given: "유저 생성"
         userInfoRepository.save(
