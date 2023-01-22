a599c41375126d95f2b6c8bf0c45d8dc0363d176
flowertaekk-dev <flowertaekk.dev@gmail.com>
2023-01-22 22:31:58 +0900

    ➕: ReadPostService interface 수정

--- src/main/java/com/dev6/rejordbe/application/post/read/ReadPostService.java
+++ src/main/java/com/dev6/rejordbe/application/post/read/ReadPostService.java
@@ -1,7 +1,9 @@
 package com.dev6.rejordbe.application.post.read;
 
 import com.dev6.rejordbe.domain.post.dto.PostResult;
+import com.dev6.rejordbe.exception.IllegalParameterException;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.time.LocalDateTime;
 
@@ -14,7 +16,9 @@ public interface ReadPostService {
      * 모든 게시글 정보 획득
      *
      * @param offsetTime {@code LocalDateTime} 최신 데이터 판단기준 시각
+     * @param pageable {@code Pageable} 페이지 정보
      * @return {@code Page<PostResult>}
+     * @throws IllegalParameterException {@code offsetTime} 이 존재하지 안흔 경우
      */
-    Page<PostResult> allPosts(final LocalDateTime offsetTime);
+    Page<PostResult> allPosts(final LocalDateTime offsetTime, final Pageable pageable);
 }
--- src/main/java/com/dev6/rejordbe/application/post/read/ReadPostServiceImpl.java
+++ src/main/java/com/dev6/rejordbe/application/post/read/ReadPostServiceImpl.java
@@ -1,20 +1,27 @@
 package com.dev6.rejordbe.application.post.read;
 
 import com.dev6.rejordbe.domain.post.dto.PostResult;
+import com.dev6.rejordbe.infrastructure.post.read.ReadPostRepository;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 
 /**
  * ReadPostServiceImpl
  */
+@Service
+@lombok.RequiredArgsConstructor
 public class ReadPostServiceImpl implements ReadPostService {
+
+    private final ReadPostRepository readPostRepository;
 
     /**
      * {@inheritDoc}
      */
     @Override
-    public Page<PostResult> allPosts(final LocalDateTime offsetTime) {
+    public Page<PostResult> allPosts(final LocalDateTime offsetTime, final Pageable pageable) {
         return null;
     }
 }
