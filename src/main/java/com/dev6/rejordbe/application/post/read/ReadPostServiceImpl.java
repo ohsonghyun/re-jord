c784fc137ddd9da399e56e50509d83ae4d6aae38
flowertaekk-dev <flowertaekk.dev@gmail.com>
2023-01-22 22:51:12 +0900

    ➕: 게시글 읽기 service 구현

--- src/main/java/com/dev6/rejordbe/application/post/read/ReadPostServiceImpl.java
+++ src/main/java/com/dev6/rejordbe/application/post/read/ReadPostServiceImpl.java
@@ -1,12 +1,16 @@
 package com.dev6.rejordbe.application.post.read;
 
+import com.dev6.rejordbe.domain.exception.ExceptionCode;
 import com.dev6.rejordbe.domain.post.dto.PostResult;
+import com.dev6.rejordbe.exception.IllegalParameterException;
 import com.dev6.rejordbe.infrastructure.post.read.ReadPostRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 /**
  * ReadPostServiceImpl
@@ -21,7 +25,13 @@ public class ReadPostServiceImpl implements ReadPostService {
      * {@inheritDoc}
      */
     @Override
-    public Page<PostResult> allPosts(final LocalDateTime offsetTime, final Pageable pageable) {
-        return null;
+    public Page<PostResult> allPosts(
+            @NonNull final LocalDateTime offsetTime,
+            @NonNull final Pageable pageable
+    ) {
+        if (Objects.isNull(offsetTime)) {
+            throw new IllegalParameterException(ExceptionCode.ILLEGAL_DATE_TIME.name());
+        }
+        return readPostRepository.searchAll(offsetTime, pageable);
     }
 }
