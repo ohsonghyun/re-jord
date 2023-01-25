1453b6cc1f577155d5e4ee9a8794d027ebb2d108
flowertaekk-dev <flowertaekk.dev@gmail.com>
2023-01-25 22:57:37 +0900

    🪛: 주석 리팩토링

--- src/test/groovy/com/dev6/rejordbe/infrastructure/post/read/ReadPostRepositorySpec.groovy
+++ src/test/groovy/com/dev6/rejordbe/infrastructure/post/read/ReadPostRepositorySpec.groovy
@@ -20,7 +20,7 @@ import javax.persistence.PersistenceContext
 import java.time.LocalDateTime
 
 /**
- * ReadPostRepositoryCustomSpec
+ * ReadPostRepositorySpec
  */
 @DataJpaTest
 @EnableJpaAuditing
