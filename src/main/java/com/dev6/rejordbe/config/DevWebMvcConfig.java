ec4f8568e027f9838a739bebb0b9c35c8ca62db0
flowertaekk-dev <flowertaekk.dev@gmail.com>
2023-01-26 21:40:38 +0900

    🪛: WebConfig 설정 변경

---
+++ src/main/java/com/dev6/rejordbe/config/DevWebMvcConfig.java
@@ -0,0 +1,35 @@
+package com.dev6.rejordbe.config;
+
+import com.dev6.rejordbe.presentation.controller.argumentResolver.LoggedInUserArgumentResolver;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+/**
+ * WebMvcConfig: 개발용
+ */
+@Profile("local")
+@Configuration
+@lombok.RequiredArgsConstructor
+public class DevWebMvcConfig implements WebMvcConfigurer {
+
+    /**
+     * CORS설정
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // 모든 Origin에 오픈. 서버에서 필요에 따라 access 제어
+        registry.addMapping("/v1/**")
+                .allowedOrigins("http://localhost:3000", "http://localhost:8080")
+                .allowCredentials(true);
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new LoggedInUserArgumentResolver());
+    }
+}
--- src/main/java/com/dev6/rejordbe/config/ProdWebMvcConfig.java
+++ src/main/java/com/dev6/rejordbe/config/ProdWebMvcConfig.java
@@ -2,6 +2,7 @@ package com.dev6.rejordbe.config;
 
 import com.dev6.rejordbe.presentation.controller.argumentResolver.LoggedInUserArgumentResolver;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,11 +10,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import java.util.List;
 
 /**
- * WebMvcConfig
+ * WebMvcConfig: PROD
  */
+@Profile("prod")
 @Configuration
 @lombok.RequiredArgsConstructor
-public class WebMvcConfig implements WebMvcConfigurer {
+public class ProdWebMvcConfig implements WebMvcConfigurer {
 
     /**
      * CORS설정
@@ -22,7 +24,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         // 모든 Origin에 오픈. 서버에서 필요에 따라 access 제어
         registry.addMapping("/v1/**")
-                .allowedOrigins("*");
+                // TODO PROD 도메인 설정
+                .allowedOrigins("*")
+                .allowCredentials(true);
     }
 
     @Override
