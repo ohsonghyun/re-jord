5f3a9aefa95fe5c24996d9a331d1109a5935d953
flowertaekk-dev <flowertaekk.dev@gmail.com>
2023-02-10 16:04:52 +0900

    🪛: 불필요한 IF 제거

--- src/main/java/com/dev6/rejordbe/infrastructure/user/UserInfoRepository.java
+++ src/main/java/com/dev6/rejordbe/infrastructure/user/UserInfoRepository.java
@@ -11,6 +11,15 @@ import java.util.Optional;
  */
 @Repository
 public interface UserInfoRepository extends JpaRepository<Users, String> {
+
+    /**
+     * 유저ID로 유저 찾기
+     *
+     * @param userId {@code String} 찾고자 하는 유저의 userId
+     * @return {@code Optional<Users>}
+     */
+    Optional<Users> findUserByUserId(final String userId);
+
     /**
      * 닉네임으로 유저 찾기
      *
@@ -18,13 +27,4 @@ public interface UserInfoRepository extends JpaRepository<Users, String> {
      * @return {@code Optional<Users>}
      */
     Optional<Users> findUserByNickname(final String nickname);
-
-    /**
-     * 유저ID and 패스워드로 유저 찾기
-     *
-     * @param userId {@code String} 찾고자 하는 유저의 userId
-     * @param password {@code String} 찾고자 하는 유저의 password
-     * @return {@code Optional<Users>}
-     */
-    Optional<Users> findUserByUserIdAndPassword(final String userId, final String password);
 }
