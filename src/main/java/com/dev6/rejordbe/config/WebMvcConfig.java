8474fd48c502f803419c31b60e4842b11be14c9d
flowertaekk-dev <flowertaekk.dev@gmail.com>
2023-02-11 01:43:47 +0900

    ⛑ : 허용 http method 설정

--- src/main/java/com/dev6/rejordbe/config/WebMvcConfig.java
+++ src/main/java/com/dev6/rejordbe/config/WebMvcConfig.java
@@ -2,7 +2,6 @@ package com.dev6.rejordbe.config;
 
 import com.dev6.rejordbe.presentation.controller.argumentResolver.LoggedInUserArgumentResolver;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,10 +11,9 @@ import java.util.List;
 /**
  * WebMvcConfig: 개발용
  */
-@Profile("local")
 @Configuration
 @lombok.RequiredArgsConstructor
-public class DevWebMvcConfig implements WebMvcConfigurer {
+public class WebMvcConfig implements WebMvcConfigurer {
 
     /**
      * CORS설정
@@ -25,6 +23,7 @@ public class DevWebMvcConfig implements WebMvcConfigurer {
         // 모든 Origin에 오픈. 서버에서 필요에 따라 access 제어
         registry.addMapping("/v1/**")
                 //.allowedOrigins("http://localhost:3000", "http://localhost:8080")
+                .allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedOrigins("*");
     }
 
