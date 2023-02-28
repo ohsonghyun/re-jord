c107be22700bfacdf1a322f41a3564f7be8c515a
ossong <imhyuninny@gmail.com>
2023-02-28 15:12:06 +0900

    :car: 코멘트 반영 주석 추가

--- src/main/java/com/dev6/rejordbe/domain/badge/BadgeCode.java
+++ src/main/java/com/dev6/rejordbe/domain/badge/BadgeCode.java
@@ -4,13 +4,14 @@ package com.dev6.rejordbe.domain.badge;
  * BadgeCode
  */
 public enum BadgeCode {
-    UNDINE,
-    VEGETARIAN,
-    ENERGY_SAVER,
-    MEAL_PLANNER,
-    DIGITAL_FAIRY,
-    FOREST_LOVER,
-    PRO_SHOPPER,
-    ENVIRONMENTAL_EXPERT,
+    UNDINE, // 워터 요정
+    VEGETARIAN, // 나름 채식러
+    ENERGY_SAVER,   // 절전 요정
+    MEAL_PLANNER,   // 식사 계획러
+    DIGITAL_FAIRY,  // 디지털 요정
+    FOREST_LOVER,   // 포레스트 러버
+    PRO_SHOPPER,    // 프로 쇼핑러
+    ENVIRONMENTAL_EXPERT,   // 환경 전문가
+    PRO_GOPHER, // 프로 부지런러
     DEFAULT
 }
