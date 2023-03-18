e7d9d3445a59c6500cfaee84a501c95b5d02059a
flowertaekk-dev <flowertaekk.dev@gmail.com>
2023-03-18 15:22:48 +0900

    Service 계층 로그 추가

--- src/main/java/com/dev6/rejordbe/application/user/validate/UserInfoValidateServiceImpl.java
+++ src/main/java/com/dev6/rejordbe/application/user/validate/UserInfoValidateServiceImpl.java
@@ -3,6 +3,7 @@ package com.dev6.rejordbe.application.user.validate;
 import com.dev6.rejordbe.domain.exception.ExceptionCode;
 import com.dev6.rejordbe.domain.role.RoleType;
 import com.dev6.rejordbe.exception.IllegalParameterException;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
@@ -15,6 +16,7 @@ import java.util.regex.Pattern;
  * UserInfoValidateServiceImpl
  */
 @Component
+@Slf4j
 public class UserInfoValidateServiceImpl implements UserInfoValidateService {
     /**
      * {@inheritDoc}
@@ -29,11 +31,13 @@ public class UserInfoValidateServiceImpl implements UserInfoValidateService {
     public boolean validateUserId(String userId, List<RuntimeException> errors) {
         boolean errorFree = true;
         if (StringUtils.isBlank(userId)) {
+            log.info("UserInfoValidateServiceImpl.validateUserId: UserId is empty or null: {}", userId);
             errors.add(new IllegalParameterException(ExceptionCode.ILLEGAL_USERID.name()));
             errorFree = false;
         } else {
             // 영문, 숫자 조합으로 5~20자 이내
             if (userId.length() < 5 || userId.length() > 20) {
+                log.info("UserInfoValidateServiceImpl.validateUserId: Wrong userId's length: {}", userId);
                 errors.add(new IllegalParameterException(ExceptionCode.ILLEGAL_USERID.name()));
                 errorFree = false;
             }
@@ -42,6 +46,7 @@ public class UserInfoValidateServiceImpl implements UserInfoValidateService {
             // 아이디가 숫자만으로 구성되어 있으면 에러
             final Pattern pattern = Pattern.compile("(^\\d+$|\\W|[A-Z])");
             if (pattern.matcher(userId).find()) {
+                log.info("UserInfoValidateServiceImpl.validateUserId: Violation against UserId rule: {}", userId);
                 errors.add(new IllegalParameterException(ExceptionCode.ILLEGAL_USERID.name()));
                 return false;
             }
@@ -60,11 +65,13 @@ public class UserInfoValidateServiceImpl implements UserInfoValidateService {
     public boolean validateNickname(String nickname, List<RuntimeException> errors) {
         boolean errorFree = true;
         if (StringUtils.isBlank(nickname)) {
+            log.info("UserInfoValidateServiceImpl.validateNickname: Nickname is empty or null: {}", nickname);
             errors.add(new IllegalParameterException(ExceptionCode.ILLEGAL_NICKNAME.name()));
             errorFree = false;
         } else {
             // 한글, 영문, 숫자, 특수문자 조합으로 2~10자 이내
             if (nickname.length() < 2 || nickname.length() > 10) {
+                log.info("UserInfoValidateServiceImpl.validateNickname: Wrong nickname's length: {}", nickname);
                 errors.add(new IllegalParameterException(ExceptionCode.ILLEGAL_NICKNAME.name()));
                 errorFree = false;
             }
@@ -72,6 +79,7 @@ public class UserInfoValidateServiceImpl implements UserInfoValidateService {
             // 영문, 한글, 숫자이외의 문자가 포함되어 있으면 에러
             Pattern pattern = Pattern.compile("[^\\w|가-힣]");
             if (pattern.matcher(nickname).find()) {
+                log.info("UserInfoValidateServiceImpl.validateNickname: Violation against Nickname rule: {}", nickname);
                 errors.add(new IllegalParameterException(ExceptionCode.ILLEGAL_NICKNAME.name()));
                 errorFree = false;
             }
@@ -86,6 +94,7 @@ public class UserInfoValidateServiceImpl implements UserInfoValidateService {
     public boolean validatePassword(String password, List<RuntimeException> errors) {
         boolean errorFree = true;
         if (StringUtils.isBlank(password)) {
+            log.info("UserInfoValidateServiceImpl.validatePassword: Password is empty: {}", password);
             errors.add(new IllegalParameterException(ExceptionCode.ILLEGAL_PASSWORD.name()));
             errorFree = false;
         }
@@ -98,12 +107,14 @@ public class UserInfoValidateServiceImpl implements UserInfoValidateService {
     @Override
     public boolean validateRoleTypes(List<String> roleNames, List<RuntimeException> errors) {
         if (CollectionUtils.isEmpty(roleNames)) {
+            log.info("UserInfoValidateServiceImpl.validateRoleTypes: roleNames is empty: {}", roleNames);
             errors.add(new IllegalParameterException(ExceptionCode.ILLEGAL_ROLE.name()));
             return false;
         }
         List<String> roleTypes = List.of(RoleType.ROLE_ADMIN, RoleType.ROLE_USER);
         for (String roleName : roleNames) {
             if (!roleTypes.contains(roleName)) {
+                log.info("UserInfoValidateServiceImpl.validateRoleTypes: Unknown roleNames: {}", roleNames);
                 errors.add(new IllegalParameterException(ExceptionCode.ILLEGAL_ROLE.name()));
                 return false;
             }
