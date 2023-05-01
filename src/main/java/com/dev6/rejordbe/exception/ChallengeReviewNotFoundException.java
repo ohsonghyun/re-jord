9abda5767fa954665e8b0ab796e8bb2e0881c6ac
ossong <imhyuninny@gmail.com>
2023-05-01 17:34:04 +0900

    :car: 챌린지 리뷰 게시글 수정 service 구현

---
+++ src/main/java/com/dev6/rejordbe/exception/ChallengeReviewNotFoundException.java
@@ -0,0 +1,10 @@
+package com.dev6.rejordbe.exception;
+
+/**
+ * ChallengeReviewNotFoundException
+ */
+public class ChallengeReviewNotFoundException extends RuntimeException {
+    public ChallengeReviewNotFoundException(String message) {
+        super(message);
+    }
+}
